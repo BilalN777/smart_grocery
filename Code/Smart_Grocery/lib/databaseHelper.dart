d2d06ecf6366b7f384a6d540fff349fed27c13d9
BilalN777 <bilal.abs.naseer@gmail.com>
2023-10-06 01:40:27 -0500

    added a sorted ingredients json and functionality for the db

--- Code/Smart_Grocery/lib/databaseHelper.dart
+++ Code/Smart_Grocery/lib/databaseHelper.dart
@@ -17,7 +17,7 @@ class DatabaseHelper {
   // add table vars here
 
   // the columns in the users.csv file
-  static final columnId = 'ID';
+  static final columnUserId = 'ID';
   static final columnName = 'Name';
   static final columnEmail = 'Email';
   static final columnPhoneNumber = 'PhoneNumber';
@@ -27,11 +27,12 @@ class DatabaseHelper {
   static final columnFoodPreferences = 'FoodPreference';
 
   // the columns in the recipe json
+  static final columnRecipeId = 'recipe_id';
   static final columnRecipeTitle = 'Recipe_title';
   static final columnInstructions = 'instructions';
   static final columnIngredients = 'ingredients';
-
-
+  static final columnIngredientId = 'ingredient_id';
+  static final columnIngredientName = 'ingredient';
 
   // Making it a Singleton
   DatabaseHelper._privateConstructor();
@@ -63,11 +64,11 @@ class DatabaseHelper {
     // users table
     await db.execute('''
           CREATE TABLE $_usersTable (
-            $columnId INTEGER PRIMARY KEY,
+            $columnUserId INTEGER PRIMARY KEY,
             $columnName TEXT NOT NULL,
             $columnEmail TEXT NOT NULL,
-            $columnPhoneNumber TEXT
-            $columnUserName TEXT NOT NULL
+            $columnPhoneNumber TEXT,
+            $columnUserName TEXT NOT NULL,
             $columnLocation TEXT,
             $columnPaymentInfo TEXT,
             $columnFoodPreferences TEXT
@@ -77,19 +78,28 @@ class DatabaseHelper {
     // recipe table
     await db.execute('''
           CREATE TABLE $_recipeTable (
-          $columnRecipeTitle TEXT PRIMARY KEY,
+          $columnRecipeId INTEGER PRIMARY KEY,
+          $columnRecipeTitle TEXT, 
           $columnInstructions TEXT, 
           $columnIngredients TEXT
           )
           ''');
 
     // ingredients table
+    // await db.execute('''
+    //       CREATE TABLE $_itemTable (
+    //       $columnRecipeId INTEGER PRIMARY KEY,
+    //       $columnRecipeTitle TEXT,
+    //       $columnIngredients TEXT,
+    //       FOREIGN KEY($columnRecipeId) REFERENCES $_recipeTable($columnRecipeId)
+    //       )
+    //       ''');
     await db.execute('''
-          CREATE TABLE $_recipeTable (
-          $columnRecipeTitle TEXT PRIMARY KEY,
-          $columnIngredients TEXT,
-          )
-          ''');
+          CREATE TABLE $_itemTable (
+          $columnIngredientId INTEGER PRIMARY KEY AUTOINCREMENT,
+          $columnIngredientName TEXT NOT NULL
+      )
+      ''');
 
   }
 
@@ -112,18 +122,85 @@ class DatabaseHelper {
   // Update: change user details
   Future<int> updateUser(Map<String, dynamic> user) async {
     Database db = await instance.database;
-    int id = user[columnId];
-    return await db.update(_usersTable, user, where: '$columnId = ?', whereArgs: [id]);
+    int id = user[columnUserId];
+    return await db.update(_usersTable, user, where: '$columnUserId = ?', whereArgs: [id]);
   }
 
   // Delete: remove a user from the DB
   Future<int> deleteUser(int id) async {
     Database db = await instance.database;
-    return await db.delete(_usersTable, where: '$columnId = ?', whereArgs: [id]);
+    return await db.delete(_usersTable, where: '$columnUserId = ?', whereArgs: [id]);
   }
 
+  // RECIPE operations
+
+  // Create: insert a recipe into the DB
+  Future<int> insertRecipe(Map<String, dynamic> recipe) async {
+    Database db = await instance.database;
+    return await db.insert(_recipeTable, recipe);
+  }
+
+  // Read: fetch all recipes from the DB
+  Future<List<Map<String, dynamic>>> queryAllRecipes() async {
+    Database db = await instance.database;
+    return await db.query(_recipeTable);
+  }
+
+  // Update: modify an existing recipe in the DB
+  Future<int> updateRecipe(Map<String, dynamic> recipe) async {
+    Database db = await instance.database;
+    int id = recipe[columnRecipeId];
+    return await db.update(_recipeTable, recipe, where: '$columnRecipeId = ?', whereArgs: [id]);
+  }
+
+  // Delete: remove a recipe from the DB
+  Future<int> deleteRecipe(int id) async {
+    Database db = await instance.database;
+    return await db.delete(_recipeTable, where: '$columnRecipeId = ?', whereArgs: [id]);
+  }
+
+  // INGREDIENT CRUD operations
+
+  // Create: insert a new ingredient into the DB
+    Future<int> insertIngredient(String ingredientName) async {
+      Database db = await instance.database;
+      return await db.insert(_itemTable, {columnIngredientName: ingredientName});
+    }
+
+  // Read: fetch all ingredients from the DB
+    Future<List<Map<String, dynamic>>> queryAllIngredients() async {
+      Database db = await instance.database;
+      return await db.query(_itemTable);
+    }
+
+  // Read: fetch a specific ingredient by its ID
+    Future<Map<String, dynamic>> queryIngredientById(int id) async {
+      Database db = await instance.database;
+      var result = await db.query(_itemTable, where: '$columnIngredientId = ?', whereArgs: [id]);
+      return result.first;
+    }
+
+  // Update: modify an existing ingredient in the DB by its ID
+    Future<int> updateIngredient(int id, String newIngredientName) async {
+      Database db = await instance.database;
+      return await db.update(
+          _itemTable,
+          {columnIngredientName: newIngredientName},
+          where: '$columnIngredientId = ?',
+          whereArgs: [id]
+      );
+    }
+
+  // Delete: remove an ingredient from the DB by its ID
+    Future<int> deleteIngredient(int id) async {
+      Database db = await instance.database;
+      return await db.delete(_itemTable, where: '$columnIngredientId = ?', whereArgs: [id]);
+    }
 
 
 
 
-}
+
+
+
+  }
